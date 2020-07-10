c963271ace5fad5d45e1349087847b05e9d53dbf
LuisEduardo21 <lfdasilva9@gmail.com>
2020-07-09 23:31:20 -0300

    subindo a primeira classe do jogo.

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,4 +1,12 @@
 package Aplication;
 
+import BoardingGame.Position;
+
 public class Program {
+
+    public static void main(String[] args){
+
+        Position position = new Position(3 , 5);
+        System.out.println(position);
+    }
 }
