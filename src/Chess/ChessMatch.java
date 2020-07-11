02212bcd3dddf027d2b74c7d3eed7ef74a02e210
LuisEduardo21 <lfdasilva9@gmail.com>
2020-07-11 17:13:00 -0300

    Subindo as atualizações adicionando o rei e a torre no jogo.

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package Chess;
 
 import BoardingGame.Board;
+import BoardingGame.Position;
+import Chess.Pieces.King;
+import Chess.Pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,6 +11,7 @@ public class ChessMatch {
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumn()];
@@ -17,5 +21,10 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+    private void initialSetup(){
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4 ));
+        board.placePiece(new King(board, Color.BLACK), new Position(7, 4 ));
     }
 }
