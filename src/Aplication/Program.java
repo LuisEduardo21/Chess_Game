a5b2b88bac7f1b46167b757863a115ef6b95189f
LuisEduardo21 <lfdasilva9@gmail.com>
2020-07-14 21:34:43 -0300

    Fazendo as movimentações das peças do jogo.

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -3,12 +3,29 @@ package Aplication;
 import BoardingGame.Board;
 import BoardingGame.Position;
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        Ui.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            Ui.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("source: ");
+            ChessPosition source = Ui.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = Ui.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
--- src/Chess/ChessException.java
+++ src/Chess/ChessException.java
@@ -1,6 +1,8 @@
 package Chess;
 
-public class ChessException extends RuntimeException{
+import BoardingGame.BoardingException;
+
+public class ChessException extends BoardingException {
     private static final long serialVersionUID = 1L;
 
     public ChessException(String msg){
