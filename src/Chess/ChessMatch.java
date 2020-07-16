14d04857c7ac3508b783bd390c453f272759f31d
LuisEduardo21 <lfdasilva9@gmail.com>
2020-07-16 20:49:43 -0300

    foram feitas que adicionar turno para cada jogador jogar um turno.

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -8,10 +8,14 @@ import Chess.Pieces.Rook;
 
 public class ChessMatch {
 
+    private int turn;
+    private Color currentPlayer;
     private Board board;
 
     public ChessMatch(){
         board = new Board(8,8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
     }
     public ChessPiece[][] getPieces(){
@@ -36,6 +40,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = MakeMove(source, target);
+        nextTurn();
         return (ChessPiece) capturedPiece;
     }
 
@@ -44,6 +49,12 @@ public class ChessMatch {
             throw new ChessException("The chosen piece can't move to target position");
 
         }
+    }
+
+    private void nextTurn(){
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+
     }
 
     private Piece MakeMove(Position source, Position target) {
@@ -56,6 +67,9 @@ public class ChessMatch {
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source Position.");
+        }
+        if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("The chosen piece is not yours");
         }
         if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen piece");
@@ -82,5 +96,13 @@ public class ChessMatch {
         placeNewPiece('e', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 8, new King(board, Color.BLACK));
 
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public Color getCurrentPlayer() {
+        return currentPlayer;
     }
 }
