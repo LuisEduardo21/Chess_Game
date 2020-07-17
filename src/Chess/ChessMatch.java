fa17595045c36eb3432ce3f346552453528f3960
LuisEduardo21 <lfdasilva9@gmail.com>
2020-07-16 22:17:37 -0300

    foram feitas alterações para o tratamento das peças capturadas.

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -6,11 +6,17 @@ import BoardingGame.Position;
 import Chess.Pieces.King;
 import Chess.Pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces =  new ArrayList<>();
 
     public ChessMatch(){
         board = new Board(8,8);
@@ -61,6 +67,11 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
+
+        if (capturedPiece != null){
+            piecesOnTheBoard.remove(capturedPiece);
+            capturedPieces.add(capturedPiece);
+        }
         return capturedPiece;
     }
 
@@ -79,6 +90,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup(){
