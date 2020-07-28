d0f18000efbbe4461f9c37e425581517b4a5ac8b
LuisEduardo21 <lfdasilva9@gmail.com>
2020-07-28 00:00:19 -0300

    Foram feitas as atualizaçoes para implementação do Bispo no jogo e seus possiveis movimentos.

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -3,6 +3,7 @@ package Chess;
 import BoardingGame.Board;
 import BoardingGame.Piece;
 import BoardingGame.Position;
+import Chess.Pieces.Bishop;
 import Chess.Pieces.King;
 import Chess.Pieces.Pawn;
 import Chess.Pieces.Rook;
@@ -169,12 +170,14 @@ public class ChessMatch {
     }
 
     private void initialSetup(){
-        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
         placeNewPiece('d', 1, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
-        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
+        placeNewPiece('a', 1, new Pawn(board, Color.WHITE));
         placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
@@ -182,8 +185,10 @@ public class ChessMatch {
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
 
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
